33cc4b28467d7aab7bf92a4c2cccade29c45cad1
bangjinhyuk <jinhyuk0103@gmail.com>
2021-11-16 20:46:52 +0900

    Create Spring boot project

---
+++ Stomp-Exam/src/main/java/com/example/stompexam/StompExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.stompexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompExamApplication.class, args);
+    }
+
+}
---
+++ Stomp-Exam/src/test/java/com/example/stompexam/StompExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stompexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
