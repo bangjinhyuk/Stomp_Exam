90037c6b82cd127ec83fd741f2cc30311dad9299
bangjinhyuk <jinhyuk0103@gmail.com>
2021-11-18 03:38:47 +0900

    fix:: #1 connect 에러

--- Stomp-Exam/src/main/java/com/example/stompexam/config/StompWebSocketConfig.java
+++ Stomp-Exam/src/main/java/com/example/stompexam/config/StompWebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.stompexam.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -19,8 +20,8 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     //이유는 왜 그런지 아직 찾지 못함
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp/chat")
-                .setAllowedOrigins("http://localhost:9091")
+        registry.addEndpoint("/stomp")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
